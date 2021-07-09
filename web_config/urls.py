7f73be9eb58089bd4ce66aea035a01fcd421f26e
00FFEF <f51.5disorder@gmail.com>
2021-07-09 17:54:06 +0900

    httpresponse 2

--- web_config/urls.py
+++ web_config/urls.py
@@ -21,4 +21,7 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', views.index),
+    path('', views.index),
+    path('index01', views.index01),
+    path('index02', views.index02),
 ]
